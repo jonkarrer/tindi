7b7edcb1cef8ca43cee56f875129b5881ee47abf
jkarrer <jkarrer@protect247.com>
2024-03-21 13:02:18 -0400

    test: rate of change

--- src/rate_of_change.rs
+++ src/rate_of_change.rs
@@ -1,8 +1,20 @@
-pub fn calculate_rate_of_change(prices: &[f32], n: usize) -> f32 {
-    if prices.len() < n {
-        panic!("Period is Longer Than Number of Prices for Rate Of Change")
+pub fn rate_of_change(prices: &[f32], periods: usize) -> Option<f32> {
+    if prices.len() < periods {
+        return None;
     }
-    let price_n_periods_ago = prices[prices.len() - n];
+    let price_n_periods_ago = prices[prices.len() - periods];
     let price_now = prices[prices.len() - 1];
-    ((price_now - price_n_periods_ago) / price_n_periods_ago) * 100.0
+
+    Some(((price_now - price_n_periods_ago) / price_n_periods_ago) * 100.0)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rate_of_change() {
+        let roc = rate_of_change(&[40.0, 43.0, 44.0, 50.0], 4).unwrap();
+        assert_eq!(roc, 25.0);
+    }
 }
